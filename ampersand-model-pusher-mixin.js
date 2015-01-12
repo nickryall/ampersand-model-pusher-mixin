d800c7380d4530976ea81d4c4bd898c35181d5cd
nickryall <me@nickryall.com.au>
2015-01-12 13:16:48 +1300

    Initial Commit

--- ampersand-model-pusher-mixin.js
+++ ampersand-model-pusher-mixin.js
@@ -65,15 +65,7 @@ module.exports = {
             var update = _.bind(function () {    
 
                 this.isLive = true;
-                // Make a shallow copy of the options object.
-                // `Backbone.collection.fetch` wraps the success function
-                // in an outer function (line `527`), replacing options.success.
-                // That means if we don't copy the object every poll, we'll end
-                // up modifying the reference object and creating callback inception.
-                //
-                // Furthermore, since the sync success wrapper
-                // that wraps and replaces options.success has a different arguments
-                // order, you'll end up getting the wrong arguments.
+
                 var opts = _.clone(options);
 
                 if (!this.tries || polledCount < this.tries) {
